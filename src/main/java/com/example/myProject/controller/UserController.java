c85fc71e3d4a18bc3045b6dea85e4910d993eec8
HakimBaidouri <hakim.baidouri@hotmail.com>
2025-04-01 14:06:50 +0200

    API Get user added

---
+++ src/main/java/com/example/myProject/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.myProject.controller;
+
+import com.example.myProject.model.User;
+import com.example.myProject.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
+
---
+++ src/main/java/com/example/myProject/model/User.java
@@ -0,0 +1,74 @@
+package com.example.myProject.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    @Column(name = "company_id")
+    private Integer companyId;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+        // Constructeur vide requis par JPA
+    }
+
+    public User(Long id, String email, String password, Integer companyId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.companyId = companyId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
