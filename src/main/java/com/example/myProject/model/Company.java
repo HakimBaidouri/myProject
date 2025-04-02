f28193b496fbbd6b3bf9f21940e8d2f74c450861
HakimBaidouri <hakim.baidouri@hotmail.com>
2025-04-02 11:42:04 +0200

    Getters and setters updated

--- src/main/java/com/example/myProject/model/Company.java
+++ src/main/java/com/example/myProject/model/Company.java
@@ -104,11 +104,14 @@ public class Company {
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
