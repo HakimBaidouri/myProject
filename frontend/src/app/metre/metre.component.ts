8609055addfcd0e9e3499a987de3af8cfc5a82be
HakimBaidouri <hakim.baidouri@hotmail.com>
2025-04-07 14:34:29 +0200

    Routing set up

---
+++ frontend/src/app/metre/metre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metre',
+  imports: [],
+  templateUrl: './metre.component.html',
+  styleUrl: './metre.component.scss'
+})
+export class MetreComponent {
+
+}
