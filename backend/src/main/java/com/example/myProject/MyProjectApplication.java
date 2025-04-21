15fb2170007a992d25395f750456a88d525ec6ef
HakimBaidouri <hakim.baidouri@hotmail.com>
2025-04-21 12:29:57 +0200

    set up VSCode for Java Spring Boot

--- backend/src/main/java/com/example/myProject/MyProjectApplication.java
+++ backend/src/main/java/com/example/myProject/MyProjectApplication.java
@@ -1,5 +1,6 @@
 package com.example.myProject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyProjectApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+                .directory("C:/Users/007/Documents/myProject/backend")
+                .filename(".env")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+			
+				
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
 }
