e9e46db6ea84ea344074b17b268eac4094029e80
HakimBaidouri <hakim.baidouri@hotmail.com>
2025-04-23 15:58:18 +0200

    link and image button added

--- frontend/src/pages/textEditor/ChapterEditorSandbox.tsx
+++ frontend/src/pages/textEditor/ChapterEditorSandbox.tsx
@@ -13,6 +13,7 @@ import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style'
 import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
+import Link from '@tiptap/extension-link'
 import Toolbar from './Toolbar'
 import { useEditorStore } from "@/store/use-editor-store"
 
@@ -52,6 +53,11 @@ export default function ChapterEditorSandbox() {
         },
         extensions: [
             StarterKit,
+            Link.configure({
+                openOnClick: false,
+                autolink: true,
+                defaultProtocol: "https"
+            }),
             Color,
             Highlight.configure({
                 multicolor: true,
